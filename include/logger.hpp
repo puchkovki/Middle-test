86476ac59b847d2ccf4030c064645de5d3cbc282
puchkovki <puchkov.k@phystech.edu>
2020-04-09 18:43:17 +0300

    Added namespace get_current

--- include/logger.hpp
+++ include/logger.hpp
@@ -7,6 +7,22 @@
 // for convenience
 using json = nlohmann::json;
 
+namespace get_current {
+	std::string time() {
+                std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+                std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+                return std::string(std::ctime(&now_time));
+        }
+
+        std::string thread_id() {
+                std::stringstream ss;
+                ss << std::this_thread::get_id();
+                return ss.str();
+        }
+}
+
+using namespace get_current;
+
 namespace output_log {
 
 template<typename T>
@@ -26,23 +42,11 @@ public:
 	auto Info(const std::string& message) -> void {
 
 		json j;
-		j["time"] = get_current_time();
+		j["time"] = time();
 		j["message"] = message;
-		j["threadId"] = get_thread_id();
+		j["threadId"] = thread_id();
 
 		m_output << j;
-	}
-	
-	std::string get_current_time() {
-		std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
-		std::time_t now_time = std::chrono::system_clock::to_time_t(now);
-		return std::string(std::ctime(&now_time));
-	}
-
-	std::string get_thread_id() {
-      		std::stringstream ss;
-	        ss << std::this_thread::get_id();
-       		return ss.str();
 	}
 };
 }
